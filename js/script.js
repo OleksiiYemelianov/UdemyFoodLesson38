9bcc5e29493771514f16a5c3814fb52651ddb383
Oleksii <nebezotveta@gmail.com>
2021-10-23 09:53:44 +0300

    end of the Lesson 54 forms

--- js/script.js
+++ js/script.js
@@ -236,7 +236,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 display: block;
                 margin: 0 auto;
             `;
-            form.append(statusMessage);
+            form.insertAdjacentElement('afterend', statusMessage);
 
             const request = new XMLHttpRequest();
             request.open('POST', 'server.php');
